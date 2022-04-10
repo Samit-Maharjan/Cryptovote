121aa1d787d606d20b06769e0e42a9cc3bf973c7
Samit-Maharjan <samitmaharjan.g@gmail.com>
2022-04-10 18:44:55 +0530

    Added Everything

---
+++ app/src/main/java/com/example/cryptovote/IndexFragment.java
@@ -0,0 +1,14 @@
+package com.example.cryptovote;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IndexFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_index, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/cryptovote/ResultFragment.java
@@ -0,0 +1,16 @@
+package com.example.cryptovote;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+
+public class ResultFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_result, container);
+    }
+}
